92700668674a68b408e283bb19e4b145e563f5c7
nepallium <122187419+nepallium@users.noreply.github.com>
2026-07-13 22:02:12 -0400

    add tmr setup 500ms period

---
+++ Modules/include/tmr.h
@@ -0,0 +1,6 @@
+#ifndef TMR_H
+#define TMR_H
+
+void tmr_init(void);
+
+#endif
--- Modules/tmr.c
+++ Modules/tmr.c
@@ -10,5 +10,10 @@
 void tmr_init(void) {
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
+	TIM2->CR1 |= (TIM_CR1_ARPE | TIM_CR1_CEN);
 
+	// APB1 Timer Clock Freq / Desired Tim Freq = Prescaler * ARR
+	// for 0.5s period (2Hz)
+	TIM2->PSC = 4200 - 1;
+	TIM2->ARR = 10,000 - 1; // datasheet says this is 32 bits
 }
